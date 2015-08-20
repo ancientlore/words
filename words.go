9ef15bff5c943a6f28e95075e88df2421a989b99
Michael Lore <michael.lore@gmail.com>
2015-08-20 16:00:32 -0400

    Added signal handling

--- words.go
+++ words.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/context"
+	"os"
+	"os/signal"
 	"regexp"
 	"sort"
 	"strings"
@@ -74,7 +76,25 @@ func print(m map[string]int) {
 }
 
 func main() {
-	ctx := context.Background()
+	// get context
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// start reader
 	rch := reader(ctx)
+
+	// handle kill signals
+	go func() {
+		// Set up channel on which to send signal notifications.
+		// We must use a buffered channel or risk missing the signal
+		// if we're not ready to receive when the signal is sent.
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, os.Kill)
+
+		// Block until a signal is received.
+		<-c
+		cancel()
+	}()
+
+	// accumulate results
 	accumulator(ctx, rch)
 }
